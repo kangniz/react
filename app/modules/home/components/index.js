932514bd4ca144cf120cdb71aded279f31078cd6
kangniz <565125761@qq.com>
2019-09-20 16:11:54 +0800

    初始化脚手架

---
+++ app/modules/home/components/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+import React from 'react'
+
+class Home extends React.Component {
+  componentWillMount() {
+  }
+
+  componentDidMount() {
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+  }
+
+  render() {
+    return (
+      <div className="container">
+        首页
+      </div>
+    )
+  }
+}
+
+export default Home
